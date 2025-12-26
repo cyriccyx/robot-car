4e6bee3446c455df394da4b874996455731dc7ee
cyriccyx <cyriccyx@gmail.com>
2025-12-26 18:44:41 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.SmallHeart)
+    makerobo.MotorRunDual(
+    makerobo.Motors.Left,
+    255,
+    makerobo.Motors.Left,
+    255
+    )
+})
